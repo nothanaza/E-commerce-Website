ca55ad20278214473cba954abfc76ffd3160737c
nothanaza <nothanazao@gmail.com>
2025-09-11 13:58:46 +0200

    changes

---
+++ components/header.php
@@ -0,0 +1,23 @@
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="assets/css/style.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css">
+</head>
+<body>
+  <header class="site-header">
+    <div class="container">
+      <div class="logo">
+        <a href="index.php">Tech Giants</a>
+      </div>
+      <nav class="main-nav">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="about.php">About</a></li>
+          <li><a href="shop.php">Shop</a></li>
+          <li><a href="contact.php">Contact</a></li>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  
